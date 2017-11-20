a1648ca657ca13d5b92be9f519dc4e1a4edb52ad
Acquisto Cyril <cyril.acquisto@gmail.com>
2017-11-21 00:45:37 +0100

    Ajout classe comportant le main

---
+++ be/acq/metier/UtilisationProgramme.java
@@ -0,0 +1,41 @@
+package be.acq.metier;
+import be.acq.clavier.*;
+
+public class UtilisationProgramme {
+	public static void start() {
+		int choix;
+		System.out.println("Bienvenue sur l'application");
+		System.out.println("Que voulez-vous faire ?");
+		System.out.println("1. Connexion");
+		System.out.println("2. Inscription");
+		System.out.println("3. Quitter");
+		do {
+			System.out.print("Votre choix (1,2,3) : ");
+			choix = Clavier.lireInt();
+			System.out.println();
+			if (choix<1 || choix >3)
+				System.out.println("Erreur d'encodage");
+		}
+		while(choix<1 || choix>3);
+		M_Personne p = new M_Personne();
+		switch(choix) {
+		case 1:
+			p.connexion();
+			break;
+		case 2:
+			p.inscription();
+			break;
+		case 3:
+			conclusion();
+			break;
+		}
+	}
+	public static void conclusion() {
+		System.out.println("Aurevoir");
+		System.exit(0);
+	}
+	
+	public static void main(String[] args) {
+		UtilisationProgramme.start();
+	}
+}
